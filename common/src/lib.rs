9cd4b0b734dc8eb096ccbe81a6f3bfd64c9cceba
mahongqin <codepapercut@gmail.com>
2024-06-04 19:40:58 +0800

    Add anyhow demo

---
+++ common/src/lib.rs
@@ -0,0 +1,4 @@
+pub use sqlx;
+pub use anyhow;
+pub use tokio;
+pub use reqwest;
--- crates/router/tests/demo.rs
+++ crates/router/tests/demo.rs
@@ -4,11 +4,31 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use common::anyhow::Result;
+    use common::reqwest;
+    use common::tokio;
+
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[tokio::test]
+    async fn test_tokio() -> Result<()> {
+        let client = reqwest::Client::new();
+        let res = client
+            .get("http://127.0.0.1:8080/api/v1/test")
+            .send()
+            .await?;
+
+        println!("resp: {}", res.status());
+
+        let body = res.text().await?;
+
+        println!("body: {}", body);
+        Ok(())
     }
 }
