b95703474cd481272fb7a58861b2861064e79fc4
mahongqin <codepapercut@gmail.com>
2024-06-15 15:17:45 +0800

    Add str trimmed example

--- common/tests/demo.rs
+++ common/tests/demo.rs
@@ -11,4 +11,11 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn str_trimmed() {
+        let str = "nvidia.com";
+        let trimmed_str = str.trim_end_matches(".com");
+        println!("{}", trimmed_str);
+    }
 }
