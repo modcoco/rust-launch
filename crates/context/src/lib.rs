2d01aa678d9f703e9983330ab711b2ff43fe9ea3
mahongqin <codepapercut@gmail.com>
2024-09-04 15:29:00 +0800

    Fix app ctx

--- crates/context/src/lib.rs
+++ crates/context/src/lib.rs
@@ -1,2 +1,25 @@
-pub mod context;
 mod postgres;
+
+use kube::{init_kube_client, kube_runtime};
+use kube_runtime::Client as KubeClient;
+use sqlx::PgPool;
+
+use crate::postgres::create_pg_pool;
+
+#[derive(Clone)]
+pub struct AppContext {
+    pub kube_client: KubeClient,
+    pub pg_pool: PgPool,
+}
+
+impl AppContext {
+    pub async fn new() -> Result<Self, anyhow::Error> {
+        let kube_client = init_kube_client().await?;
+        let pg_pool = create_pg_pool().await;
+
+        Ok(Self {
+            kube_client,
+            pg_pool,
+        })
+    }
+}
--- crates/router/src/lib.rs
+++ crates/router/src/lib.rs
@@ -1,11 +1,9 @@
 use axum::{routing::get, Extension, Router};
-use context::context::{KubeContext, PgContext};
+use context::AppContext;
 
 pub async fn init_router() -> Result<Router, anyhow::Error> {
-    let kube_ctx = KubeContext::new().await?;
-    let pg_ctx = PgContext::new().await;
+    let ctx = AppContext::new().await?;
     Ok(Router::new()
         .route("/health", get(|| async { "Hello, World!" }))
-        .layer(Extension(kube_ctx))
-        .layer(Extension(pg_ctx)))
+        .layer(Extension(ctx)))
 }
