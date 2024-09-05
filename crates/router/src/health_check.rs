6381e6e5f8aa381e8763cd182978eb02f445660c
mahongqin <codepapercut@gmail.com>
2024-09-06 00:57:26 +0800

    Fix refresh processes

--- crates/router/src/health_check.rs
+++ crates/router/src/health_check.rs
@@ -16,14 +16,14 @@ impl SystemInfo {
         sys.refresh_all();
 
         thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-        sys.refresh_processes_specifics(
-            ProcessesToUpdate::All,
-            ProcessRefreshKind::new().with_cpu(),
-        );
 
         let cpu_count = sys.cpus().len();
         let pid = sysinfo::get_current_pid().expect("Failed to get current PID");
         let total_memory_gb = format!("{:.2}Gi", sys.total_memory() as f32 / 1_073_741_824.0); // 1 GB = 1024^3
+        sys.refresh_processes_specifics(
+            ProcessesToUpdate::Some(&[pid]),
+            ProcessRefreshKind::new().with_cpu(),
+        );
         let (process_cpu_usage, process_memory_mb) =
             sys.process(pid)
                 .map_or(("unknown".to_string(), "unknown".to_string()), |process| {
