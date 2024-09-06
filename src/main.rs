b679326b98d1333696e50c91811f398da252533d
mahongqin <codepapercut@gmail.com>
2024-09-06 11:02:47 +0800

    Fix show build info

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use axum::{self};
 use logger::logger_trace::init_logger;
 use router::init_router;
-use rust_boot::utils::build::show_build_info;
+use rust_boot::utils;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let (_handle, _guard) = init_logger("rust-boot", true);
-    show_build_info();
+    utils::build::show_build_info();
     let cfg = config::get_config();
     let router = init_router().await?;
     let addr = format!("{}:{}", cfg.web_listen_addr, cfg.web_listen_port);
