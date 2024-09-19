3b3278c0e4052fd4cd8543df476e8b1078ec0328
mahongqin <codepapercut@gmail.com>
2024-09-19 20:24:18 +0800

    Add insert value build

--- crates/data_store/tests/query.rs
+++ crates/data_store/tests/query.rs
@@ -1,8 +1,18 @@
 use chrono::NaiveDateTime;
+use data_store::GetFieldNames;
 use serde::{Deserialize, Serialize};
 use sqlx::{PgPool, QueryBuilder};
 
 const BIND_LIMIT: usize = 65535;
+
+#[macro_export]
+macro_rules! generate_push_binds {
+    ($query_builder:expr, $user:expr, [$($field:ident),*]) => {
+        $(
+            $query_builder.push_bind($user.$field);
+        )*
+    };
+}
 
 #[tokio::test]
 async fn test_query() -> anyhow::Result<()> {
@@ -17,7 +27,7 @@ async fn test_query() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize, GetFieldNames)]
 pub struct User {
     pub id: i32,
     pub username: String,
@@ -45,12 +55,18 @@ async fn insert_users_build(
     pool: &PgPool,
     users: impl Iterator<Item = User>,
 ) -> Result<(), sqlx::Error> {
-    let mut query_builder = QueryBuilder::new("INSERT INTO users(username, email, created_at) ");
+    let field_names: Vec<&str> = User::field_names()
+        .iter()
+        .filter(|&&field| field != "id")
+        .copied()
+        .collect();
+    let insert_value_unit = field_names.join(", ");
+
+    let mut query_builder =
+        QueryBuilder::new(format!("INSERT INTO users ({}) ", insert_value_unit));
 
     query_builder.push_values(users.take(BIND_LIMIT / 4), |mut b, user| {
-        b.push_bind(user.username)
-            .push_bind(user.email)
-            .push_bind(user.created_at);
+        generate_push_binds!(b, user, [username, email, created_at]);
     });
 
     let query = query_builder.build();
@@ -75,11 +91,11 @@ async fn test_add_user() -> anyhow::Result<()> {
 async fn test_add_users() -> anyhow::Result<()> {
     let pool = PgPool::connect(&dotenvy::var("DATABASE_URL")?).await?;
     let users = (0..10).map(|i| User {
-        id: i,
         username: format!("test_user_{i}"),
         email: format!("test-user-{i}@example.com"),
         created_at: NaiveDateTime::parse_from_str("2024-09-19 12:00:00", "%Y-%m-%d %H:%M:%S")
             .unwrap_or_default(),
+        ..Default::default()
     });
 
     insert_users_build(&pool, users).await?;
